0daadeb8d8b42e1663ee4f479ad4174b15e3852b
PapaDenke <73295800+PapaDenke@users.noreply.github.com>
2020-11-13 18:25:34 +0100

    Add files via upload

---
+++ Arduino/ESP32_CrashChecker/ESP32_CrashChecker.ino
@@ -0,0 +1,121 @@
+#include <WiFi.h>
+#include <I2Cdev.h>
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <Wire.h>
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+
+
+// constants to connect to web
+const char *ssid="myGate";
+const char *password="192837465";
+
+// constants to connect to server
+const uint16_t port= 5555;
+const char *host ="192.168.137.1";//"192.168.0.207";
+
+MPU6050 mpu(0x68);
+
+void setup() {
+  Serial.begin(115200);
+    while (!Serial);
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(-56); //(220);
+    mpu.setYGyroOffset(39); //(76);
+    mpu.setZGyroOffset(-15); //(-85);
+    mpu.setZAccelOffset(2925); //(1788);
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(6);
+        mpu.CalibrateGyro(6);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+  // connect to WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("...");
+  }
+ 
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void fetchCoordinates(){
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+          mpu.dmpGetGravity(&gravity, &q);
+          mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+          // display real acceleration, adjusted to remove gravity
+          mpu.dmpGetAccel(&aa, fifoBuffer);
+          mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+}
+
+void aufbereitung(){
+
+}
+
+void auswertungDiskret(){
+ 
+}
+
+void auswertungStetig(){
+
+}
+
+void output(){
+
+}
