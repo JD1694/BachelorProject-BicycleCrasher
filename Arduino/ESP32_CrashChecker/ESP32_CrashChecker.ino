b7fbee41c58055522fd098379a4c5963526602fc
Max-Ole von Waldow <maxolevonwaldow@gmail.com>
2020-12-09 18:22:41 +0100

    added more thresholds for evaluation of data. Added first concept of static evaluation with the norm of the distance between reading and threshold vector, an exponential average as a safe case to compare a reading to and a sigmoin funktion for percentage output.

--- Arduino/ESP32_CrashChecker/ESP32_CrashChecker.ino
+++ Arduino/ESP32_CrashChecker/ESP32_CrashChecker.ino
@@ -5,12 +5,45 @@
 
 // Switch between evaluation modes
 #define EVAL_DISCRETE
-#define EVAL_CONTINUOUS
+//#define EVAL_CONTINUOUS
 
 // Parameters to tune
 float GYRO_EXP_DECLINE_FACTOR = 0.1;
-// Threshold  to tune
-float THRESHOLD_SMOOTH_GYRO = 1;
+int   FIFO_INT_LENGTH_BIG = 100;
+int   FIFO_INT_LENGTH_SHORT = 40;
+// Thresholds  to tune
+float THRESHOLD_SMOOTH_GYRO_X = 1;
+float THRESHOLD_SMOOTH_GYRO_Y = 1;
+float THRESHOLD_SMOOTH_GYRO_Z = 1;
+float THRESHOLD_YAW = 360;
+float THRESHOLD_PITCH = 50;
+float THRESHOLD_ROLL = 50;
+int   THRESHOLD_INT_ACCEL_X = 50;
+int   THRESHOLD_INT_ACCEL_Y = 50;
+int   THRESHOLD_INT_ACCEL_Z = 50;
+int   THRESHOLD_INT_GRAVITY_X = 1;
+int   THRESHOLD_INT_GRAVITY_Y = 1;
+int   THRESHOLD_INT_GRAVITY_Z = 1;
+int   COMMON_GRAVITY_Z = 1000;
+// continuous calc variables
+float  ypr_eval;
+float  gyro_eval;
+int    accel_eval;
+int    gravity_eval;
+double delta_x;
+double delta_y;
+double delta_z;
+double continuous_rating = 0.0;
+double normalized_continuous_rating = 0.0;
+
+// Further processed data
+VectorInt16 accelIntegral;
+VectorInt16 accelGravityIntegral;
+VectorFloat gyroSmoothend;
+
+// return from evaluation
+bool   crashYesNo = false;
+double crashPropability = 0.0;
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -30,10 +63,6 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// Further processed data
-VectorInt16 accelIntegral;
-VectorFloat gyroSmoothend;
-
 // constants to connect to web
 const char *ssid = "myGate";
 const char *password = "192837465";
@@ -41,10 +70,6 @@ const char *password = "192837465";
 // constants to connect to server
 const uint16_t port = 5555;
 const char *host = "192.168.137.1"; //"192.168.0.207";
-
-// return from evaluation
-bool crashYesNo = false;
-float crashPropability = 0.0;
 
 MPU6050 mpu(0x68);
 
@@ -110,31 +135,36 @@ void setup() {
 
 void loop() {
   // Get sensor readings
-  fetchCoordinates();
-  
+  getSensorReadings();
+
   // Prepare data, filter and convert to get more measurable input
   prepareData();
-  
+
   // Evaluate inputs
-  #ifdef EVAL_DISCRETE
-    crashYesNo = evalDiscrete();
-    if (crashYesNo)
-      {crashPropability = 1.0;}
-    else
-      {crashPropability = 0.0;}
-  #endif
-  #ifdef EVAL_CONTINUOUS
-    crashPropability = evalContinuous();
-  #endif
+#ifdef EVAL_DISCRETE
+  crashYesNo = evalDiscrete();
+  if (crashYesNo)
+  {
+    crashPropability = 1.0;
+  }
+  else
+  {
+    crashPropability = 0.0;
+  }
+#endif
+
+#ifdef EVAL_CONTINUOUS
+  crashPropability = evalContinuous();
+#endif
 
   // Reaction
   if (crashPropability > 0.5) {
     output(crashPropability);
   }
-    
+
 }
 
-void fetchCoordinates() {
+void getSensorReadings() {
   // Get sensor readings from IMU
   mpu.dmpGetQuaternion(&q, fifoBuffer);
   mpu.dmpGetGravity(&gravity, &q);
@@ -147,26 +177,52 @@ void fetchCoordinates() {
 void prepareData() {
   /* Prepare data, filter and convert to get more measurable input */
 
-  // add new readings to collection (FIFO?)
-  //// fifo_aaReal.add(aaReal); 
+  // add new readings to collection (FIFO of length FIFO_INT_LENGTH_BIG)
+  //// fifo_aaReal.add(aaReal);
   //// last_aaReal = fifo_aaReal.pop();
-  
+
   // integrate acceleration over the last fraction of a second: add newest, remove oldest value
-  /*
-  accelIntegral.x = accelIntegral.x + aaReal.x - last_aaReal.x;
-  accelIntegral.y = accelIntegral.y + aaReal.y - last_aaReal.y;
-  accelIntegral.z = accelIntegral.z + aaReal.z - last_aaReal.z;
+  /*https://github.com/rlogiacco/CircularBuffer
+    accelIntegral.x = accelIntegral.x + aaReal.x - last_aaReal.x;
+    accelIntegral.y = accelIntegral.y + aaReal.y - last_aaReal.y;
+    accelIntegral.z = accelIntegral.z + aaReal.z - last_aaReal.z;
+    // Accel with Gravity
+    accelGravityIntegral.x = accelGravityIntegral.x + aa.x - last_aa.x;
+    accelGravityIntegral.y = accelGravityIntegral.y + aa.y - last_aa.y;
+    accelGravityIntegral.z = accelGravityIntegral.z + aa.z - last_aa.z;
   */
-  
+
   // Filter gyro data (exponetial decline as time proceeds)
-  gyroSmoothend.x = gyroSmoothend.x*(1-GYRO_EXP_DECLINE_FACTOR) + gy.x*GYRO_EXP_DECLINE_FACTOR;
-  gyroSmoothend.y = gyroSmoothend.y*(1-GYRO_EXP_DECLINE_FACTOR) + gy.y*GYRO_EXP_DECLINE_FACTOR;
-  gyroSmoothend.z = gyroSmoothend.z*(1-GYRO_EXP_DECLINE_FACTOR) + gy.z*GYRO_EXP_DECLINE_FACTOR;
+  gyroSmoothend.x = gyroSmoothend.x * (1 - GYRO_EXP_DECLINE_FACTOR) + gy.x * GYRO_EXP_DECLINE_FACTOR;
+  gyroSmoothend.y = gyroSmoothend.y * (1 - GYRO_EXP_DECLINE_FACTOR) + gy.y * GYRO_EXP_DECLINE_FACTOR;
+  gyroSmoothend.z = gyroSmoothend.z * (1 - GYRO_EXP_DECLINE_FACTOR) + gy.z * GYRO_EXP_DECLINE_FACTOR;
 }
 
 bool evalDiscrete() {
   /* Evaluate inputs into discrete categories: Crash and Safe */
-  if (abs(gyroSmoothend.x) > THRESHOLD_SMOOTH_GYRO){
+
+  // Absolute Angle
+  if ( abs(ypr[0] * 180 / M_PI) > THRESHOLD_YAW ///// evtl y-p-r vertauscht
+       || abs(ypr[1] * 180 / M_PI) > THRESHOLD_PITCH ///// change to rad for more efficienty
+       || abs(ypr[2] * 180 / M_PI) > THRESHOLD_ROLL) {
+    return true;
+  }
+  // Rate of rotation
+  if ( abs(gyroSmoothend.x) > THRESHOLD_SMOOTH_GYRO_X
+       || abs(gyroSmoothend.y) > THRESHOLD_SMOOTH_GYRO_Y
+       || abs(gyroSmoothend.z) > THRESHOLD_SMOOTH_GYRO_Z) {
+    return true;
+  }
+  // Acceleration
+  if ( abs(accelIntegral.x) > THRESHOLD_INT_ACCEL_X
+       || abs(accelIntegral.y) > THRESHOLD_INT_ACCEL_Y
+       || abs(accelIntegral.z) > THRESHOLD_INT_ACCEL_Z) {
+    return true;
+  }
+  // Direction of Gravity and Momentum
+  if ( abs(accelGravityIntegral.x) > THRESHOLD_INT_GRAVITY_X
+       || abs(accelGravityIntegral.y) > THRESHOLD_INT_GRAVITY_Y
+       || abs(accelGravityIntegral.z - COMMON_GRAVITY_Z) > THRESHOLD_INT_GRAVITY_Z) {
     return true;
   }
   // ...
@@ -175,14 +231,59 @@ bool evalDiscrete() {
   return false;
 }
 
-float evalContinuous() {
+double evalContinuous() {
   /* Evaluate inputs into continuous categories: Propability of Crash */
+  /* Notes: square may not have methods in arg*/
   //THRESHOLD_SMOOTH_GYRO - abs(gyroSmoothend.x)
-  return 0.0;
+
+  // Absolute Angle
+  delta_x = abs(ypr[0] * 180 / M_PI) - THRESHOLD_YAW; ///// evtl y-p-r vertauscht///// change to rad for more efficienty
+  delta_y = abs(ypr[0] * 180 / M_PI) - THRESHOLD_PITCH;
+  delta_z = abs(ypr[0] * 180 / M_PI) - THRESHOLD_ROLL;
+  ypr_eval = norm(delta_x, delta_y, delta_z);
+
+  // Rate of rotation
+  delta_x = abs(gyroSmoothend.x) - THRESHOLD_SMOOTH_GYRO_X;
+  delta_y = abs(gyroSmoothend.y) - THRESHOLD_SMOOTH_GYRO_Y;
+  delta_z = abs(gyroSmoothend.z) - THRESHOLD_SMOOTH_GYRO_Z;
+  gyro_eval = norm(delta_x, delta_y, delta_z);
+
+  // Acceleration
+  delta_x = abs(accelIntegral.x) > THRESHOLD_INT_ACCEL_X;
+  delta_y = abs(accelIntegral.y) > THRESHOLD_INT_ACCEL_Y;
+  delta_z = abs(accelIntegral.z) > THRESHOLD_INT_ACCEL_Z;
+  accel_eval = norm(delta_x, delta_y, delta_z);
+
+  // Direction of Gravity and Momentum
+  delta_x = abs(accelGravityIntegral.x) > THRESHOLD_INT_GRAVITY_X;
+  delta_y = abs(accelGravityIntegral.y) > THRESHOLD_INT_GRAVITY_Y;
+  delta_z = abs(accelGravityIntegral.z - COMMON_GRAVITY_Z) > THRESHOLD_INT_GRAVITY_Z;
+  gravity_eval = norm(delta_x, delta_y, delta_z);
+
+  // combine and normalize
+  continuous_rating = ypr_eval + gyro_eval + accel_eval + gravity_eval;
+  normalized_continuous_rating = normalized_continuous_rating*0.999 + continuous_rating*0.001; // assumed as safe ride
+
+  // convert to useable and capped percentage result
+  return sigmoid_function(continuous_rating/normalized_continuous_rating - 1);
 }
 
 void output(float crashPropability) {
   /* Reaction if a crash is detected */
   Serial.print("Crash detected having propability: ");
-  Serial.println(crashPropability*100);
+  Serial.println(crashPropability * 100);
+  // Send WIFI Distress signal to extentions
+}
+
+double norm(double a, double b, double c){
+  /* Calculates the euclidian norm of a vektor consisting of the 3 given values*/
+  return sqrt( sq(delta_x) + sq(delta_y) + sq(delta_z) );
+}
+
+double sigmoid_function(double x){
+  /* 
+   *  A fast implementation (without exp(-x)) of the Sigmoid activation curve, modified to return return percentages.
+   *  Returns results between 0 (for very negative x) and 1 (for very positive x) 
+   *  centered around x=0 where 0.5 is returned*/
+   return x / (2*(1 + abs(x))) + 0.5;
 }
